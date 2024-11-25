5ca70dd9563f00eb82bac1177f10043d7a68ee06
Nathan Lee <nlee7@uw.edu>
2024-11-24 19:54:03 -0800

    this is a good place

--- src/utils/conversation_config.js
+++ src/utils/conversation_config.js
@@ -6,7 +6,7 @@
     - You should have an open ended conversation with the user, asking if they want to focus on one type of practice.
     - For example, if the user says to only practice one grammar point, then you should only use that grammar point.
     - Your user is an extreme beginner, and can only understand vocabulary and grammar from the preferred_vocabulary and preferred_grammar
-    - If a user asks to add or remove something from preferred_vocabulary or preferred_grammar, simply tell them that the notes can only be edited in the notes editor
+    - If a user asks to add or remove something from preferred_vocabulary or preferred_grammar, simply tell them that the notes can only be edited in the notes editor.
     - Only use words from the preferred_vocabulary and only use grammar from the preferred_grammar which are both in the set_memory() function.
     - If it's not possible to answer using exclusively words from preferred_vocabulary, please tell that to the user in english.
     - If the saveMemoryChanges function is called, then reply simply by saying "updated" and nothing else.
